b84f55eae675f420cece7699bbc886380c1d24b7
piotrekks <91351744+piotrekks@users.noreply.github.com>
2023-11-05 19:10:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,63 @@
 #include <iostream>
-#include <windows.h>
-#include <cstdlib>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-string imie;
-string odp;
+// Structure representing a single question with answer options
+struct Question {
+    string questionText;
+    vector<string> answerOptions;
+    int correctAnswerIndex;
+};
 
-int main()
-{
-    //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
-    cout << "\aPodaj imie: ";
-    cin >> imie;
-    int pkt = 0;
+// Function to ask a question and check the answer
+bool askQuestion(const Question& question) {
+    cout << question.questionText << endl;
 
-    system("cls");
-    cout<< "3";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2..1..";
-    Sleep(1000);
-    system("cls");
-
-    cout << imie << " witaj w tescie z programowania\n";
-
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
-    cout << "Twoja odpowiedz: ";
-    cin >> odp;
-    if(odp == "b")
-    {
-        cout << "Poprawna odpowiedz!";
-        pkt++;
+    for (size_t i = 0; i < question.answerOptions.size(); i++) {
+        cout << i + 1 << ". " << question.answerOptions[i] << endl;
     }
-    else
-    {
-        cout << "Zle! Jest to jezyk programowania!";
 
+    int userAnswerIndex;
+    cout << "Wybierz numer odpowiedzi: ";
+    cin >> userAnswerIndex;
+
+    if (userAnswerIndex >= 1 && userAnswerIndex <= question.answerOptions.size()) {
+        return (userAnswerIndex - 1 == question.correctAnswerIndex);
     }
-    Sleep(3000);
-    system("cls");
+    else {
+        cout << "Niepoprawny numer odpowiedzi." << endl;
+        return false;
+    }
+}
 
+int main() {
+    int score = 0;
 
+    // Array of questions
+    Question questions[] = {
+        {"Jaka jest stolica Francji?", {"Paryz", "Berlin", "Londyn", "Rzym"}, 0},
+        {"Ile wynosi 4 * 5?", {"15", "20", "25", "30"}, 1},
+        {"Kto jest autorem dramatu 'Hamlet'?", {"Shakespeare", "Dostojewski", "Hemingway", "Twain"}, 0},
+        {"Ktory kraj jest znany jako 'Kraina Kwitnacej Wisni'?", {"Japonia", "Chiny", "Indie", "Korea Południowa"}, 0},
+        {"Jakie jezioro jest najwieksze na swiecie?", {"Bajkał", "Morze Kaspijskie", "Jezioro Michigan", "Jezioro Wiktorii"}, 0},
+        {"Ile planet jest w naszym Ukladzie Slonecznym?", {"6", "7", "8", "9"}, 2}
+    };
+
+    for (int i = 0; i < 6; i++) {
+        if (askQuestion(questions[i])) {
+            cout << "Poprawna odpowiedz! Zdobywasz punkt." << endl;
+            cout << endl;
+            score++;
+        }
+        else {
+            cout << "Niepoprawna odpowiedz." << endl;
+            cout << endl;
+        }
+    }
+
+    cout << "Twoj wynik to: " << score << "/6 punktow." << endl;
 
     return 0;
 }
